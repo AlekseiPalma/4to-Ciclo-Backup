4dd82f127355f300d5fb28963640562d428cc215
Aleksei Palma <supersonic616@hotmail.com>
2018-09-30 12:37:08 -0500

    proyecto agregado

---
+++ Proto/Assets/Scripts/Runner/Counter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+	public Text Coins;
+
+	public int Score;
+
+	public static Counter instance;
+
+	void Awake()
+	{
+		instance = this;	
+	}
+
+	// Use this for initialization
+	void Start () {
+
+        Score = GlobalControl.Instance.Score;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Coins.text = "x " + Score.ToString();
+        if (Score < 0)
+        {
+            Score = 0;
+        }
+	}
+}
